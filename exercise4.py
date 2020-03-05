457da6680f5b72fd59c4a66ee48bb6b6aacfdf63
Mitya <lakritze@yandex.ru>
2020-03-05 16:40:16 +0300

    completed exercise4

--- exercise4.py
+++ exercise4.py
@@ -3,3 +3,14 @@
 Найдите самую большую цифру в числе.
 Для решения используйте цикл while и арифметические операции.
 """
+
+my_num = int(input("Введите целое положительное число: "))
+my_num2 = my_num
+largest = 0
+while my_num > 0:
+    r = my_num % 10
+    if largest < r:
+        largest = r
+    my_num = int(my_num / 10)
+
+print("Самая большая цифра в числе", my_num2, "— это", largest)
