30bc4b09f3738a8c560a05872a9ed8d3aef364da
Mitya <lakritze@yandex.ru>
2020-03-15 18:30:02 +0300

    lesson 4

--- exercise1.py
+++ exercise1.py
@@ -2,3 +2,16 @@
 необходимо использовать формулу: (выработка в часах * ставка в час) + премия. Для выполнения расчета для конкретных
 значений необходимо запускать скрипт с параметрами."""
 
+from sys import argv
+
+script_name, first_param, second_param, third_param = argv
+
+work_hours = float(first_param)
+hourly_wage = float(second_param)
+bonus = float(third_param)
+pay = work_hours * hourly_wage + bonus
+
+print('Выработка в часах:', work_hours)
+print('Ставка в час:', hourly_wage)
+print('Премия:', bonus)
+print('Заработная плата составляет:', pay)
