7c67d471de74977ebdf4770e6d78d118c1bf50ac
Mitya <lakritze@yandex.ru>
2020-03-19 16:56:09 +0300

    lesson 5

--- exercise1.py
+++ exercise1.py
@@ -1,3 +1,12 @@
 """1. Создать программно файл в текстовом формате, записать в него построчно данные, вводимые пользователем. Об
 окончании ввода данных свидетельствует пустая строка."""
 
+f = open('my_file1.txt', 'w')
+f.close()
+
+with open('my_file1.txt', 'a', encoding='utf-8') as f_obj:
+    my_var = 1
+    while my_var:
+        my_var = input('Введите строку: ')
+        f_obj.write(my_var)
+        f_obj.write('\n')
