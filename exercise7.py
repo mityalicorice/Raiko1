3aee5b0c52382f83aabf75ba0e485ee5cbfe5d2f
Mitya <lakritze@yandex.ru>
2020-03-19 22:28:10 +0300

    lesson 5

--- exercise7.py
+++ exercise7.py
@@ -12,3 +12,46 @@
 
 Подсказка: использовать менеджеры контекста."""
 
+import json
+
+with open('my_file7.txt', 'r', encoding='utf-8') as f_obj:
+
+    a = f_obj.readlines()  # Сохраняется каждая строка как элемент списка 'a'
+
+    line_count = len(open('my_file7.txt').readlines())  # Считаем количество организаций в файле (по количеству строк)
+
+    all_firms = []  # В этом списке будут все фирмы, в т.ч. убыточные
+
+    all_profits = []  # В этом списке будут прибыли и убытки
+
+    profitable_firms = []  # В этом списке будут только фирмы без убытка
+
+    only_profits = [] # В этом списке будут прибыли только прибыльных фирм
+
+    i = 0
+    while i < int(line_count):
+        x = a[i].split(' ')
+        name = x[0]  # Название компании
+        profit = float(x[2]) - float(x[3])  # Прибыль организации (выручка минус издержки)
+        print('Название:', name, '    Прибыль:', profit)  # Выводим название и прибыль
+        all_firms.append(name)
+        all_profits.append(profit)
+        if profit > 0:
+            profitable_firms.append(name)
+            only_profits.append(profit)
+        i += 1
+
+    mid_profit = sum(map(float, only_profits)) / len(profitable_firms)  # Средняя прибыль (без учёта убыточных компаний)
+
+    print('Средняя прибыль:', mid_profit)
+
+    firms_and_profits = dict(zip(all_firms, all_profits))  # Словарь с фирмами и их прибылями
+    mid_profit_dict = {'average_profit' : mid_profit}  # Словарь со средней прибылью
+    firms_profits_midprofit = [firms_and_profits, mid_profit_dict]  # Список содержит два словаря
+
+    with open('my_file7_data.json', 'w', encoding='utf-8') as file:
+        json.dump(firms_profits_midprofit, file, ensure_ascii=False)  # Список сохраняется в виде json-объекта в файл
+
+    with open('my_file7_data.json', 'r', encoding='utf-8') as file2:
+        resurrect = json.load(file2)  # Воскрешается список из json-файла
+        print(resurrect)
