4812e895f68ed99b5697ec06d429867c685de1b3
Mitya <lakritze@yandex.ru>
2020-03-19 18:56:31 +0300

    lesson 5

--- exercise2.py
+++ exercise2.py
@@ -2,8 +2,6 @@
 количества слов в каждой строке."""
 
 with open('my_file2.txt', 'r', encoding='utf-8') as f_obj:
-    # content = f_obj.read()
-    # print(content)
     line_count = len(open('my_file2.txt').readlines())
     print('Всего в файле', line_count, 'строк')
     result = [len(line.split(' ')) for line in f_obj]
